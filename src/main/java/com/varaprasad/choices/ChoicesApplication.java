3a27037b92efce82af43f96d529a823a376b07e9
varaprasad <pothulavaraprasad333@gmail.com>
2022-03-27 17:09:45 +0530

    initial commit

---
+++ src/main/java/com/varaprasad/choices/ChoicesApplication.java
@@ -0,0 +1,11 @@
+package com.varaprasad.choices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChoicesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/varaprasad/choices/ChoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varaprasad.choices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+@DataMongoTest
+class ChoicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
